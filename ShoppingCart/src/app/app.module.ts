748322dd9e90e7686b756e70104952dfe649cb42
Lenovo-PC\Aisha <akulindwa@mum.edu>
2017-09-25 16:58:42 -0500

    Adding login with firebase
    adding the login component that implements firebase

--- ShoppingCart/src/app/app.module.ts
+++ ShoppingCart/src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
+// for auth
+import {AngularFireAuthModule} from 'angularfire2/auth';
+// for database
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import { LoginComponent } from './login/login.component';
 
 export const firebaseConfig = {
   apiKey: "AIzaSyCw01ZeyKRn_limNemPKmF9aLNanHvNZUg",
@@ -14,11 +21,17 @@ export const firebaseConfig = {
 };
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
